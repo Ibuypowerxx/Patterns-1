e3602d96824d2252106441d88de035a4d5777a3f
ibuypower <danroz1995@yandex.ru>
2024-04-02 21:11:29 +0300

    Failed

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -1,6 +1,7 @@
 package ru.netology.data;
 
 import com.github.javafaker.Faker;
+import lombok.Value;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -58,60 +59,11 @@ public class DataGenerator {
         }
     }
 
-    public static final class UserInfo {
-        private final String city;
-        private final String name;
-        private final String phone;
-
-        public UserInfo(String city, String name, String phone) {
-            this.city = city;
-            this.name = name;
-            this.phone = phone;
-        }
-
-        public String getCity() {
-            return this.city;
-        }
-
-        public String getName() {
-            return this.name;
-        }
-
-        public String getPhone() {
-            return this.phone;
-        }
-
-        public boolean equals(final Object o) {
-            if (o == this) return true;
-            if (!(o instanceof UserInfo)) return false;
-            final UserInfo other = (UserInfo) o;
-            final Object this$city = this.getCity();
-            final Object other$city = other.getCity();
-            if (!Objects.equals(this$city, other$city)) return false;
-            final Object this$name = this.getName();
-            final Object other$name = other.getName();
-            if (!Objects.equals(this$name, other$name)) return false;
-            final Object this$phone = this.getPhone();
-            final Object other$phone = other.getPhone();
-            if (!Objects.equals(this$phone, other$phone)) return false;
-            return true;
-        }
-
-        public int hashCode() {
-            final int PRIME = 59;
-            int result = 1;
-            final Object $city = this.getCity();
-            result = result * PRIME + ($city == null ? 43 : $city.hashCode());
-            final Object $name = this.getName();
-            result = result * PRIME + ($name == null ? 43 : $name.hashCode());
-            final Object $phone = this.getPhone();
-            result = result * PRIME + ($phone == null ? 43 : $phone.hashCode());
-            return result;
-        }
-
-        public String toString() {
-            return "DataGenerator.UserInfo(city=" + this.getCity() + ", name=" + this.getName() + ", phone=" + this.getPhone() + ")";
-        }
+   @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
     }
 }
 
